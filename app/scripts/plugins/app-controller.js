8a6b664131a58a5387331370b743687d06ca922e
tam.nm <tam.nm@sutrixmedia.com>
2016-06-02 15:51:33 +0700

    Update source code.

--- app/scripts/plugins/app-controller.js
+++ app/scripts/plugins/app-controller.js
@@ -1,16 +1,21 @@
 angular.module('my-app')
-  .controller('AppCtrl', function($scope, $mdDialog) {
-    $scope.showAlert = showAlert;
+  .controller('AppCtrl', AppCtrl);
 
-    function showAlert() {
-      // var parent = angular.element(document.getElementById('popupContainer'));
-      var options = {
-        title : 'This is title',
-        textContent : 'This is contents',
+AppCtrl.$inject = ['$mdDialog'];
+function AppCtrl($mdDialog) {
+
+  var vm = this;
+  console.log(vm);
+  vm.showAlert = showAlert;
+
+  function showAlert() {
+    var options = {
+        title: 'This is title',
+        textContent: 'This is contents',
         ok: 'Got it'
       },
       aleart = $mdDialog.alert(options);
-      $mdDialog.show(aleart);
-    }
+    $mdDialog.show(aleart);
+  }
 
-  });
+}
